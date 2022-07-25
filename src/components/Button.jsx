9db6535d1d7b05b6ed8e3af34c5bf35c4fcb099d
Joosep Arrak <jooseparrak@gmail.com>
2022-07-26 00:19:10 +0300

    Added min-width to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ icon, label, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="pl-2 pr-6 h-12 btn flex justify-between items-center"
+      className="min-w-[97px] pl-2 pr-6 h-12 btn flex justify-between items-center"
     >
       <img src={icon} alt={label} className="h-8 mr-3" />
       {label}
